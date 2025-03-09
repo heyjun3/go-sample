a156ad22ee8aab6c7ea7fd110975632ac587d3b9
heyjun3 <t3k2g5kako@yahoo.co.jp>
2025-03-10 00:52:26 +0900

    update2

--- score.go
+++ score.go
@@ -49,21 +49,38 @@ func NewScore(a, b, c, d, e int) (*Score, error) {
 	}, nil
 }
 
-type Rule func(*Score, *Score) uint8
+type Rule interface {
+	Fn(*Score, *Score) uint8
+	Boundary() uint8
+}
 
-func DisengageAdult(first, second *Score) uint8 {
+type DisengageAdult struct{}
+
+func (d DisengageAdult) Fn(first, second *Score) uint8 {
 	if math.Abs(float64(first.c)-float64(second.c)) < 16 {
 		return 2
 	}
 	return 0
 }
-func IsCritical(first, second *Score) uint8 {
+func (d DisengageAdult) Boundary() uint8 {
+	return 2
+}
+
+type IsCritical struct{}
+
+func (i IsCritical) Fn(first, second *Score) uint8 {
 	if first.a >= 5 && second.a >= 5 {
 		return 0
 	}
 	return 1
 }
-func IsFree(first, second *Score) uint8 {
+func (i IsCritical) Boundary() uint8 {
+	return 1
+}
+
+type IsFree struct{}
+
+func (i IsFree) Fn(first, second *Score) uint8 {
 	if math.Abs(float64(first.d)-float64(first.e)) < 2 {
 		return 4
 	}
@@ -78,7 +95,13 @@ func IsFree(first, second *Score) uint8 {
 	}
 	return 0
 }
-func IsAdaptive(first, second *Score) uint8 {
+func (i IsFree) Boundary() uint8 {
+	return 4
+}
+
+type IsAdaptive struct{}
+
+func (i IsAdaptive) Fn(first, second *Score) uint8 {
 	max := uint8(0)
 	for _, v := range []uint8{first.a, first.b, first.c, first.d, first.e} {
 		if max < v {
@@ -89,6 +112,9 @@ func IsAdaptive(first, second *Score) uint8 {
 		return 1
 	}
 	return 0
+}
+func (i IsAdaptive) Boundary() uint8 {
+	return 1
 }
 
 type CompatibilityCalculator struct {
@@ -101,19 +127,19 @@ func NewCompatibilityCalculator(rules ...Rule) *CompatibilityCalculator {
 	}
 }
 
-func (c *CompatibilityCalculator) calcCompatibility(first, second *Score) uint8 {
+func (c *CompatibilityCalculator) calcCompatibility(first, second *Score, max uint8) uint8 {
 	var result uint8 = 0
 	for _, rule := range c.rules {
-		result += rule(first, second)
+		result += rule.Fn(first, second)
 	}
 	return result
 }
 
 func (c *CompatibilityCalculator) MostMatchingCompatibility(managers []*User, member *User) *User {
 	max := uint8(0)
-	matching := make([]*User, 0, len(managers))
+	matching := make([]*User, 0)
 	for _, manager := range managers {
-		result := c.calcCompatibility(manager.score, member.score)
+		result := c.calcCompatibility(manager.score, member.score, 0)
 		if max < result {
 			max = result
 			matching = []*User{manager}
@@ -121,10 +147,25 @@ func (c *CompatibilityCalculator) MostMatchingCompatibility(managers []*User, me
 			matching = append(matching, manager)
 		}
 	}
+
 	return matching[rand.Int63n(int64(len(matching)))]
 }
 
 func (c *CompatibilityCalculator) ExecMatching(managers, members []*User) map[string][]string {
+	m := make(map[string][]string)
+	for _, member := range members {
+		match := c.MostMatchingCompatibility(managers, member)
+		if v, ok := m[match.ID]; ok {
+			v = append(v, member.ID)
+			m[match.ID] = v
+		} else {
+			m[match.ID] = []string{member.ID}
+		}
+	}
+	return m
+}
+
+func (c *CompatibilityCalculator) ExecMatchingConcurrency(managers, members []*User) map[string][]string {
 	m := make(map[string][]string)
 	ch := make(chan string, len(members))
 	go func() {
--- score_test.go
+++ score_test.go
@@ -5,18 +5,16 @@ import (
 	"math/rand"
 	"strconv"
 	"testing"
+	"time"
 
 	gosample "github.com/heyjun3/go-sample"
 )
 
-func TestCompatibilityCalculator(t *testing.T) {
-	calculator := gosample.NewCompatibilityCalculator(
-		gosample.DisengageAdult,
-		gosample.IsCritical,
-		gosample.IsFree,
-		gosample.IsAdaptive,
-	)
-	c := 70000
+func calculatorFactory(t *testing.T, c int) (
+	*gosample.CompatibilityCalculator,
+	[]*gosample.User,
+	[]*gosample.User,
+) {
 	managers := make([]*gosample.User, 0, c)
 	members := make([]*gosample.User, 0, c)
 	for i := range c {
@@ -45,13 +43,25 @@ func TestCompatibilityCalculator(t *testing.T) {
 		}
 		members = append(members, member)
 	}
+	calculator := gosample.NewCompatibilityCalculator(
+		gosample.DisengageAdult{},
+		gosample.IsCritical{},
+		gosample.IsFree{},
+		gosample.IsAdaptive{},
+	)
+	return calculator, managers, members
+}
 
+func TestCompatibilityCalculator(t *testing.T) {
+	calculator, managers, members := calculatorFactory(t, 50000)
+	start := time.Now()
 	calculator.ExecMatching(managers, members)
-	fmt.Println("done")
+	fmt.Println("done exec matching.", time.Since(start).Round(time.Millisecond))
+}
 
-	// result := calculator.ExecMatching(managers, members)
-	// fmt.Println(result)
-	// for k, v := range result {
-	// 	fmt.Println("key", k, "value", v)
-	// }
+func TestCompatibilityCalculatorConcurrency(t *testing.T) {
+	calculator, managers, members := calculatorFactory(t, 20000)
+	start := time.Now()
+	calculator.ExecMatchingConcurrency(managers, members)
+	fmt.Println("done exec matching concurrency.", time.Since(start).Round(time.Millisecond))
 }
